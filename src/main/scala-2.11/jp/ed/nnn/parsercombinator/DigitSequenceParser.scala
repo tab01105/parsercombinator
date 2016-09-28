92531c551889af6e3d19266d434541dff08966f8
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-09-28 18:28:55 +0900

    Add DigitSequenceParser

---
+++ src/main/scala-2.11/jp/ed/nnn/parsercombinator/DigitSequenceParser.scala
@@ -0,0 +1,12 @@
+package jp.ed.nnn.parsercombinator
+
+case class DigitSequence(seq: Seq[String])
+
+object DigitSequenceParser extends MyFirstCombinator  {
+
+  def digit: Parser[String] = oneOf('0' to '9')
+
+  def apply(input: String): ParseResult[DigitSequence] =
+    map(rep(digit), { list: List[String] => DigitSequence(list) })(input)
+
+}
--- src/main/scala-2.11/jp/ed/nnn/parsercombinator/MyFirstCombinator.scala
+++ src/main/scala-2.11/jp/ed/nnn/parsercombinator/MyFirstCombinator.scala
@@ -53,6 +53,21 @@ abstract class MyFirstCombinator {
     }
   }
 
+  def rep[T](parser: Parser[T]): Parser[List[T]] = input => {
+
+    def repeatRec(input: String): (List[T], String) = parser(input) match {
+      case Success(value, next1) =>
+        val (result, next2) = repeatRec(next1)
+        (value::result, next2)
+      case Failure =>
+        (Nil, input)
+    }
+
+    val (result, next) = repeatRec(input)
+    Success(result, next)
+  }
+
+
   def map[T, U](parser: Parser[T], function: T => U): Parser[U] = input => {
     parser(input) match {
       case Success(value, next) => Success(function(value), next)
