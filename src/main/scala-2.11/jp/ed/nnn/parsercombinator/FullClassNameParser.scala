087ca86bca4190fc64c9cebbbb0b8f59ef29ffe7
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-09-28 18:17:39 +0900

    Add FullClassNameParser

---
+++ src/main/scala-2.11/jp/ed/nnn/parsercombinator/FullClassNameParser.scala
@@ -0,0 +1,16 @@
+package jp.ed.nnn.parsercombinator
+
+case class FullClassName(grade: String, className: String)
+
+object FullClassNameParser extends MyFirstCombinator {
+
+  def grade: Parser[String] = oneOf('1' to '3')
+
+  def className: Parser[String] = oneOf('A' to 'D')
+
+  def apply(input: String): ParseResult[FullClassName] =
+    map(combine(combine(combine(grade, s("年")), className), s("組")), {
+      t: (((String, String), String), String) => FullClassName(t._1._1._1, t._1._2)
+    })(input)
+
+}
