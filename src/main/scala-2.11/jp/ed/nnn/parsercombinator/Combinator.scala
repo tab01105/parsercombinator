27b4c7be0e555ef31e676ed712df57ab9135bfe4
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-09-29 20:06:51 +0900

    Add sep

--- src/main/scala-2.11/jp/ed/nnn/parsercombinator/Combinator.scala
+++ src/main/scala-2.11/jp/ed/nnn/parsercombinator/Combinator.scala
@@ -23,6 +23,20 @@ abstract class Combinator {
     */
   def s(literal: String): Parser[String] = string(literal)
 
+  def rep[T](parser: Parser[T]): Parser[List[T]] = input => {
+
+    def repeatRec(input: String): (List[T], String) = parser(input) match {
+      case Success(value, next1) =>
+        val (result, next2) = repeatRec(next1)
+        (value::result, next2)
+      case Failure =>
+        (Nil, input)
+    }
+
+    val (result, next) = repeatRec(input)
+    Success(result, next)
+  }
+
   implicit class RichParser[T](val parser: Parser[T]) {
 
     /**
