ef9b0e068c0d2d88eecc3644faf48edaf93c50b1
tab01105 <tab01105@gmail.com>
2018-01-16 13:53:18 +0900

    AirthParserの実装

---
+++ src/main/scala-2.11/jp/ed/nnn/parsercombinator/ArithParser.scala
@@ -0,0 +1,21 @@
+package jp.ed.nnn.parsercombinator
+
+import jp.ed.nnn.parsercombinator.JSONParser.{s, value}
+
+object ArithParser extends Combinator {
+
+  def expr: Parser[Any] = {
+    term ~ rep((s("+") ~ term) | (s("-") ~ term))
+  }
+
+  def term: Parser[Any] = {
+    factor  ~ rep((s("*") ~ factor) | (s("/") ~ factor))
+  }
+
+  def factor: Parser[Any] = {
+    floatingPointNumber |
+      (s("(") ~ expr ~ s(")"))
+  }
+
+  def apply(input: String): Any = expr(input)
+}
